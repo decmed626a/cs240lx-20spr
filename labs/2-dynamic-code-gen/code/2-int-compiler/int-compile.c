40c8c0d974e2a68cc4d595441c5aac686dca2e23
Brian Jun <brianjun@Brians-MacBook-Pro-808.local>
2020-04-17 20:57:37 -0700

    feat: add jump threading

--- labs/2-dynamic-code-gen/code/2-int-compiler/int-compile.c
+++ labs/2-dynamic-code-gen/code/2-int-compiler/int-compile.c
@@ -3,14 +3,12 @@
 
 #define NELEM(x) (sizeof(x) / sizeof((x)[0]))
 #include "cycle-util.h"
-#include "interrupt_handlers.c"
 
 typedef void (*int_fp)(void);
 
-//static volatile unsigned cnt = 0;
+static volatile unsigned cnt = 0;
 
 // fake little "interrupt" handlers: useful just for measurement.
-#if 0
 void int_0() { cnt++; }
 void int_1() { cnt++; }
 void int_2() { cnt++; }
@@ -19,9 +17,8 @@ void int_4() { cnt++; }
 void int_5() { cnt++; }
 void int_6() { cnt++; }
 void int_7() { cnt++; }
-#endif 
 
-static uint32_t code [16] __attribute__((aligned(16)));
+static uint32_t code [128] __attribute__((aligned(16)));
 static uint8_t m;
 
 void int_compile(int_fp* intv, unsigned n) {
@@ -34,12 +31,37 @@ void int_compile(int_fp* intv, unsigned n) {
 	}
 	code[m++] = arm_pop(arm_pc);
 
+	/*
 	printk("emitted code: \n");
 	for(int i = 0; i < m; i++) {
 		printk("code[%d]=0x%x\n", i, code[i]);
 	}
+	*/
 }
 
+static inline void* get_pc(void)  {
+    void *pc;
+    asm("mov %0, pc" : "=r"(pc));
+    return pc;
+}
+
+void jump_thread_compile(int_fp* intv, unsigned n) {
+	
+	uint32_t bx_lr_code = arm_bx(arm_lr);
+	
+	for(int i = 0; i < n; i++) {
+		int_fp* curr_inst = (int_fp*)intv[i];
+		while(memcmp((void*)(&bx_lr_code), (void*)(curr_inst), sizeof(uint32_t)) != 0) {
+			curr_inst++;
+		}
+		
+		if(i != n-1) {
+			uint32_t new_inst = arm_ldr_word_imm(0, arm_pc, intv[i+1]);
+			*curr_inst = new_inst;
+		}
+	}
+
+}
 
 void generic_call_int(int_fp *intv, unsigned n) { 
     for(unsigned i = 0; i < n; i++)
@@ -75,7 +97,7 @@ void notmain(void) {
     unsigned n = NELEM(intv);
 
 	int_compile(intv, n);
-	
+
     // try with and without cache: but if you modify the routines to do 
     // jump-threadig, must either:
     //  1. generate code when cache is off.
@@ -96,35 +118,12 @@ void notmain(void) {
     TIME_CYC_PRINT10("cost of dynamic int calling", dynamic_call_int() );
     demand(cnt == n*10, "cnt=%d, expected=%d\n", cnt, n*10);
 
-#if 0
+	jump_thread_compile(intv, n);
+	
 	cnt = 0;
-	void (*dynamic_call_int)(void) = (typeof(dynamic_call_int))code;
-    TIME_CYC_PRINT10("cost of dynamic int calling", dynamic_call_int() );
+    TIME_CYC_PRINT10("cost of jump threading", int_0() );
     demand(cnt == n*10, "cnt=%d, expected=%d\n", cnt, n*10);
-#endif
-    clean_reboot();
+    
+	clean_reboot();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
