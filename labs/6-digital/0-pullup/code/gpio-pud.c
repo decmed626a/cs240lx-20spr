1fdb1e4af28272ec09263bf68359e083f3cc852b
Brian Jun <brianjun@Brians-MacBook-Pro-808.local>
2020-04-30 18:51:57 -0700

    feat: get pullup working

--- labs/6-digital/0-pullup/code/gpio-pud.c
+++ labs/6-digital/0-pullup/code/gpio-pud.c
@@ -1,6 +1,40 @@
 // put your pull-up/pull-down implementations here.
 #include "rpi.h"
+#include "cs140e-src/cycle-count.h"
 
-void gpio_set_pullup(unsigned pin) { unimplemented(); }
+volatile unsigned *GPPUD = (void*) 0x20200094;
+volatile unsigned *GPPUDCLK0 = (void*) 0x20200098;
+volatile unsigned *GPPUDCLK1 = (void*) 0x2020009C;
+
+void gpio_set_pullup(unsigned pin) { 
+	unsigned val = (0x2); 
+	put32(GPPUD, val);
+	//printk("Wrote to GPPUD\n");
+	unsigned start = cycle_cnt_read();
+	while(cycle_cnt_read() - start < 150) {}
+	
+	volatile unsigned* pud_clk_reg = 0;
+	if(pin <=31) {
+		pud_clk_reg = GPPUDCLK0;
+	} else {
+		pud_clk_reg = GPPUDCLK1;
+	}
+	//printk("Determined clock_reg\n");
+	unsigned pud_offset = pin & 0x1f;
+	put32(pud_clk_reg, (1 << pud_offset));
+	//printk("Wrote clock reg\n");
+	start = cycle_cnt_read();
+	while(cycle_cnt_read() - start < 150) {}
+	
+	put32(GPPUD, 0);
+	//printk("cleared GPPUD\n");
+	start = cycle_cnt_read();
+	while(cycle_cnt_read() - start < 150) {}
+
+	put32(pud_clk_reg, 0);
+	//printk("cleared clk reg\n");
+	start = cycle_cnt_read();
+	while(cycle_cnt_read() - start < 150) {}
+}
 void gpio_set_pulldown(unsigned pin) { unimplemented(); }
 void gpio_pud_off(unsigned pin) { unimplemented(); }
--- labs/6-digital/0-pullup/code/on.c
+++ labs/6-digital/0-pullup/code/on.c
@@ -1,16 +1,17 @@
 // trivial program to set pin 21 to a pullup, 20 to a pulldown.
 #include "rpi.h"
+#include "cs140e-src/cycle-count.h"
 
 void notmain(void) {
     uart_init();
-
+	cycle_cnt_init();
     unsigned up = 21, down = 20;
 
     // i don't believe we need to set input (since the state is sticky)
     gpio_set_input(up);
     gpio_set_pullup(up);
     printk("pullup=%d, this is sticky: touch red lead to this pin and black to ground: should see a ~3V reading on multimeter\n", up);
-
+	while(1){}
     gpio_set_input(up);
     gpio_set_pulldown(down);
     printk("pulldown=%d, this is sticky: touch black lead to this pin, and red to 3v: should see a ~3V reading on multimeter\n", down);
