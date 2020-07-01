b1ea2a1eede8574908099e3c36e012915ac1ff56
Brian Jun <brianjun@Brians-MacBook-Pro-808.local>
2020-06-30 18:20:14 -0700

    chore: copy over lab16 contents

---
+++ labs/17-memcheck/part1-single-step/code/cp14-debug.c
@@ -0,0 +1,287 @@
+// handle debug exceptions.
+#include "rpi.h"
+#include "rpi-interrupts.h"
+#include "libc/helper-macros.h"
+#include "cp14-debug.h"
+#include "bit-support.h"
+
+/******************************************************************
+ * part1 set a watchpoint on 0.
+ )*/
+static handler_t watchpt_handler0 = 0;
+
+cp_asm_set(prefetch_flush, p15, 0, c7, c5, 4)
+
+// prefetch flush.
+#define prefetch_flush() prefetch_flush_set(0)
+
+// cp14_asm_get(wfar, c6, 0)
+
+//static inline uint32_t cp14_wcr0_get(void) {
+//    unimplemented();
+//}
+
+cp14_asm_get(wcr0, c0, 7)
+
+//static inline void cp14_wcr0_set(uint32_t r) {
+//    unimplemented();
+//}
+
+cp14_asm_set(wcr0, c0, 7) 
+
+//static inline uint32_t cp14_wvr0_get(void) { unimplemented(); }
+
+cp14_asm_get(wvr0, c0, 6)
+
+//static inline void cp14_wvr0_set(uint32_t r) { unimplemented(); }
+
+cp14_asm_set(wvr0, c0, 6)
+
+//static inline uint32_t cp14_status_get(void) {
+//    unimplemented();
+//}
+
+cp14_asm_get(dscr, c1, 0) 
+
+//static inline void cp14_status_set(uint32_t v) {
+//    unimplemented();
+//}
+
+cp14_asm_set(dscr, c1, 0)
+
+
+// set the first watchpoint: calls <handler> on debug fault.
+void watchpt_set0(void *_addr, handler_t handler) {
+	
+	// Read WCR0
+	uint32_t wcr0_reg = wcr0_get();
+	
+	// Disable watchpoint	
+	wcr0_reg &= ~(1 << 0);
+
+	// Write back to WCR0
+	wcr0_set(wcr0_reg);
+
+	assert(wcr0_reg == wcr0_get());
+	
+	uint32_t wvr0_val = (uint32_t)_addr;
+
+	// Write address to WVR 
+	wvr0_set(wvr0_val);
+
+	wcr0_reg = 0;
+	wcr0_reg = (0b0 << 20) |
+			   (0b00 << 14) |
+			   (0b1111 << 5) |
+			   (0b11 << 3) |
+			   (0b11 << 1) | 
+			   (0b1 << 0);
+
+	wcr0_set(wcr0_reg);
+   
+	assert(wcr0_reg == wcr0_get());
+
+	prefetch_flush();
+    watchpt_handler0 = handler;
+}
+
+// check for watchpoint fault and call <handler> if so.
+void data_abort_vector(unsigned pc) {
+    static int nfaults = 0;
+    //printk("nfault=%d: data abort at %p\n", nfaults++, pc);
+    if(datafault_from_ld()) {
+       // printk("was from a load\n");
+	 }
+    else {
+        //printk("was from a store\n");
+	}
+	if(!was_debug_datafault()) 
+        panic("impossible: should get no other faults\n");
+
+    // this is the pc
+    //printk("wfar address = %p, pc = %p\n", wfar_get()-8, pc);
+    assert(wfar_get()-4 == pc);
+
+    assert(watchpt_handler0);
+    
+    uint32_t addr = far_get();
+    //printk("far address = %p\n", addr);
+
+    // should send all the registers so the client can modify.
+    watchpt_handler0(0, pc, addr);
+}
+
+void cp14_enable(void) {
+    static int init_p = 0;
+
+    if(!init_p) { 
+        int_init();
+        init_p = 1;
+    }
+
+    // for the core to take a debug exception, monitor debug mode has to be both 
+    // selected and enabled --- bit 14 clear and bit 15 set.
+	uint32_t v = 0;
+	v = (1 << 15);
+	dscr_set(v);
+
+    prefetch_flush();
+    // assert(!cp14_watchpoint_occured());
+
+}
+
+/**************************************************************
+ * part 2
+ */
+
+static handler_t brkpt_handler0 = 0;
+
+cp14_asm_get(cp14_bvr0, c0, 4)
+cp14_asm_set(cp14_bvr0, c0, 4)
+cp14_asm_get(cp14_bcr0, c0, 5)
+cp14_asm_set(cp14_bcr0, c0, 5)
+//static inline uint32_t cp14_bvr0_get(void) { unimplemented(); }
+//static inline void cp14_bvr0_set(uint32_t r) { unimplemented(); }
+//static inline uint32_t cp14_bcr0_get(void) { unimplemented(); }
+//static inline void cp14_bcr0_set(uint32_t r) { unimplemented(); }
+
+static unsigned bvr_match(void) { return 0b00 << 21; }
+static unsigned bvr_mismatch(void) { return 0b10 << 21; }
+
+static inline uint32_t brkpt_get_va0(void) {
+    return cp14_bvr0_get();
+}
+static void brkpt_disable0(void) {
+	uint32_t bcr0_reg = 0;
+	bcr0_reg = (0b00 << 21) |
+			   (0b0 << 20) |
+			   (0b00 << 14) |
+			   (0b1111 << 5) |
+			   (0b11 << 1) | 
+			   (0b0 << 0);
+
+	cp14_bcr0_set(bcr0_reg);
+}
+
+// 13-16
+// returns the 
+void brkpt_set0(uint32_t addr, handler_t handler) {
+	uint32_t bcr0_reg = cp14_bcr0_get();
+
+	// Disable watchpoint	
+	bcr0_reg &= ~(1 << 0);
+
+	// Write back to BCR0
+	cp14_bcr0_set(bcr0_reg);
+
+	uint32_t bvr0_val = (uint32_t)addr;
+
+	// Write address to WVR 
+	cp14_bvr0_set(bvr0_val);
+
+	bcr0_reg = 0;
+	bcr0_reg = (0b00 << 21) |
+			   (0b0 << 20) |
+			   (0b00 << 14) |
+			   (0b1111 << 5) |
+			   (0b11 << 1) | 
+			   (0b1 << 0);
+
+	cp14_bcr0_set(bcr0_reg);
+	prefetch_flush();
+	brkpt_handler0 = handler;
+}
+
+// if get a breakpoint call <brkpt_handler0>
+void prefetch_abort_vector(unsigned pc) {
+    //printk("prefetch abort at %p\n", pc);
+    if(!was_debug_instfault())
+        panic("impossible: should get no other faults\n");
+    assert(brkpt_handler0);
+    //printk("After assert\n", pc);
+    brkpt_handler0(0, pc, ifar_get());
+}
+
+void interrupt_vector(unsigned pc) {
+    
+}
+
+/**************************************************************
+ * part 2
+ */
+
+int brk_verbose_p = 0;
+
+// used by trampoline to catch if the code returns.
+void brk_no_ret_error(void) {
+    panic("returned and should not have!\n");
+}
+
+void brkpt_mismatch_set0(uint32_t addr, handler_t handler) {
+	uint32_t bcr0_reg = cp14_bcr0_get();
+
+	// Disable watchpoint	
+	bcr0_reg &= ~(1 << 0);
+
+	// Write back to BCR0
+	cp14_bcr0_set(bcr0_reg);
+
+	uint32_t bvr0_val = (uint32_t)addr;
+
+	// Write address to WVR 
+	cp14_bvr0_set(bvr0_val);
+
+	bcr0_reg = 0;
+	bcr0_reg = (0b10 << 21) |  // Set to 0b10 for address mismatch
+			   (0b0 << 20) |
+			   (0b00 << 14) |
+			   (0b1111 << 5) |
+			   (0b11 << 1) | 
+			   (0b1 << 0);
+
+	cp14_bcr0_set(bcr0_reg);
+	prefetch_flush();
+	brkpt_handler0 = handler;
+}
+
+// should be this addr.
+void brkpt_mismatch_disable0(uint32_t addr) {
+	uint32_t bcr0_reg = 0;
+	bcr0_reg = (0b10 << 21) |  // Set to 0b10 for address mismatch
+			   (0b0 << 20) |
+			   (0b00 << 14) |
+			   (0b1111 << 5) |
+			   (0b11 << 1) | 
+			   (0b0 << 0);
+	cp14_bcr0_set(bcr0_reg);
+}
+
+// <pc> should point to the system call instruction.
+//      can see the encoding on a3-29:  lower 24 bits hold the encoding.
+// <r0> = the first argument passed to the system call.
+//
+// system call numbers:
+//  <1> - set spsr to the value of <r0>
+int syscall_vector(unsigned pc, uint32_t r0) {
+    uint32_t sys_num;
+    // figure out the instruction and the system call number.
+    sys_num = *((unsigned * ) pc) & 0xFFFFFF;
+    if(sys_num == 1){
+        asm volatile ("msr spsr, r1");
+    }else if(sys_num == 2){
+		// if can take lock, return 1
+		if(*(int*)r0 == 0) {
+			*(int*)r0 = 1;
+			return 1;
+		} else {
+			return 0;
+		}
+	}else if (sys_num == 3) {
+		return 3;
+		//printk("result: %x\n", r0);
+    }else{
+        printk("illegal system call %d\n", sys_num);
+    }
+
+    return 0;
+}
---
+++ labs/17-memcheck/part1-single-step/code/tests/16-part2-test6.c
@@ -0,0 +1,62 @@
+// trivial check example: increments variable without having a lock.
+// will fail.
+#include "check-interleave.h"
+#include "sys-lock.h"
+
+// trivial counter.   broken.
+static volatile int cnt = 0;
+static lock_t l;
+static volatile int B_retry = 0;
+
+static void cnt_A(checker_t *c) { 
+    if(!sys_lock_try(&l))
+        panic("impossible\n");
+    cnt++; 
+    sys_unlock(&l);
+} 
+static void cnt_B(checker_t *c) { 
+    if(!sys_lock_try(&l))
+        B_retry = 1;
+    else {
+        cnt++; 
+        sys_unlock(&l);
+    }
+}
+
+static void cnt_init(checker_t *c) { 
+    cnt = 0; 
+    B_retry = 0;
+    sys_lock_init(&l);
+}
+static int  cnt_check(checker_t *c) { 
+    if(B_retry) {
+        B_retry = 0;
+        assert(l == 0);
+        c->B(c);
+        assert(!B_retry);
+    }
+    return cnt == 2; 
+}
+
+checker_t cnt_mk_checker(void) {
+    return (struct checker) { 
+        .state = 0,
+        .A = cnt_A,
+        .B = cnt_B,
+        .init = cnt_init,
+        .check = cnt_check
+    };
+}
+
+void notmain(void) {
+    // try with enable cache.
+    enable_cache();
+
+    struct checker c = cnt_mk_checker();
+    int n;
+    if(!check(&c))
+        panic("check should NOT have failed!\n");
+    else 
+        exit_success("check succeeded as it should have, ntrials=%d, nerrors=%d\n", 
+                c.ntrials, c.nerrors);
+}
